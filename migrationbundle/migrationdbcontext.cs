a154569bcaa6afc3b0f53077be0f6b0f04354c44
ehn-rd <nandwaharry24@gmail.com>
2025-02-08 10:24:18 +0300

    check if there is a pending migration

--- migrationbundle/migrationdbcontext.cs
+++ migrationbundle/migrationdbcontext.cs
@@ -7,6 +7,7 @@ public class Migrationdbcontext: DbContext
 {
     public DbSet<Todo> Todos { get; set; }
     public DbSet<Category> Categories { get; set; }
+    public DbSet<Workflows> Workflows { get; set; }
 
     public Migrationdbcontext(DbContextOptions<Migrationdbcontext> options)
         : base(options)
